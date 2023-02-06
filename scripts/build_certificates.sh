985a60d3171dd32e8cf4297be9fe9a818b3cf5ec
agruhl <gruhl.alexander@gmail.com>
2023-02-06 23:48:15 +0100

    Update build_certificates.sh

--- scripts/build_certificates.sh
+++ scripts/build_certificates.sh
@@ -2,44 +2,51 @@
 
 set -e
 
-mkdir -p {certs/ca,certs/server,certs/jwt,certs/clients}
+mkdir certs
+cd certs
 
 echo ""
 echo "----------------------------------------------------"
 echo "[1/6] Create CA"
 echo "----------------------------------------------------"
-openssl req -nodes -newkey rsa:4096 -keyout certs/clients/unauthorized_client.key -out certs/clients/unknown_client.crt -subj "/C=GB/ST=London/L=London/O=Some Other Global Security/OU=IT Department/CN=The authority"
+
+mkdir ca
+openssl req -x509 -new -newkey rsa:4096 -nodes -sha256 -days 1826 -keyout ca/ca.key -out ca/ca.crt -subj "/C=GB/ST=London/L=London/O=Some Other Global Security/OU=IT Department/CN=The authority"
+
 
 echo ""
 echo "----------------------------------------------------"
 echo "[2/6] Create server certificate"
 echo "----------------------------------------------------"
-openssl req -nodes -newkey rsa:2048 -keyout certs/server/server.key -out certs/server/server.csr -subj "/C=GB/ST=London/L=London/O=Global Security/OU=IT Department/CN=server"
-openssl x509 -req -days 365 -in certs/server/server.csr -CA certs/ca/ca.crt -CAkey certs/ca/ca.key -set_serial 01 -out certs/server/server.crt
+mkdir server
+openssl req -nodes -newkey rsa:2048 -keyout server/server.key -out server/server.csr -subj "/C=GB/ST=London/L=London/O=Global Security/OU=IT Department/CN=server"
+openssl x509 -req -days 365 -in server/server.csr -CA ca/ca.crt -CAkey ca/ca.key -CAcreateserial -out server/server.crt
 
 echo ""
 echo "----------------------------------------------------"
 echo "[3/6] Create client certificate [CN: authorized_client]"
 echo "----------------------------------------------------"
-openssl req -nodes -newkey rsa:2048 -keyout certs/clients/client.key -out certs/clients/client.csr -subj "/C=GB/ST=London/L=London/O=Global Security/OU=IT Department/CN=JackAuthorized"
-openssl x509 -req -days 365 -in certs/clients/client.csr -CA certs/ca/ca.crt -CAkey certs/ca/ca.key -set_serial 01 -out certs/clients/client.crt
+mkdir clients
+openssl req -nodes -newkey rsa:2048 -keyout clients/client.key -out clients/client.csr -subj "/C=GB/ST=London/L=London/O=Global Security/OU=IT Department/CN=JackAuthorized"
+openssl x509 -req -days 365 -in clients/client.csr -CA ca/ca.crt -CAkey ca/ca.key -CAcreateserial -out clients/client.crt
 
 echo ""
 echo "----------------------------------------------------"
 echo "[4/6] Create unauthorized client certificate [CN: unauthorized]"
 echo "----------------------------------------------------"
-openssl req -nodes -newkey rsa:2048 -keyout certs/clients/unauthorized_client.key -out certs/clients/unauthorized_client.csr -subj "/C=GB/ST=London/L=London/O=Global Security/OU=IT Department/CN=MickUnauthorized"
-openssl x509 -req -days 365 -in certs/clients/unauthorized_client.csr -CA certs/ca/ca.crt -CAkey certs/ca/ca.key -set_serial 01 -out certs/clients/unauthorized_client.crt
+openssl req -nodes -newkey rsa:2048 -keyout clients/unauthorized_client.key -out clients/unauthorized_client.csr -subj "/C=GB/ST=London/L=London/O=Global Security/OU=IT Department/CN=MickUnauthorized"
+openssl x509 -req -days 365 -in clients/unauthorized_client.csr -CA ca/ca.crt -CAkey ca/ca.key -set_serial 01 -out clients/unauthorized_client.crt
 
 echo ""
 echo "----------------------------------------------------"
 echo '[5/6] Create "unknown" client certificate [CN: authorized_client]'
 echo "----------------------------------------------------"
-openssl req -nodes -newkey rsa:2048 -keyout certs/clients/unauthorized_client.key -out certs/clients/unknown_client.crt -subj "/C=GB/ST=London/L=London/O=Some Other Global Security/OU=IT Department/CN=Whoaru?"
+openssl req -nodes -newkey rsa:2048 -keyout clients/unknown_client.key -out clients/unknown_client.crt -subj "/C=GB/ST=London/L=London/O=Some Other Global Security/OU=IT Department/CN=Whoaru?"
 
 echo ""
 echo "----------------------------------------------------"
 echo "[6/6] Create cert for signing JWTs"
 echo "----------------------------------------------------"
-openssl req -nodes -newkey rsa:2048 -keyout certs/jwt/trusted_jwt_signer.key -out certs/jwt/trusted_jwt_signer.csr -subj "/C=GB/ST=London/L=London/O=Global Security/OU=IT Department/CN=JWTSigner"
-openssl x509 -req -days 365 -in certs/jwt/trusted_jwt_signer.csr -CA certs/ca/ca.crt -CAkey certs/ca/ca.key -set_serial 01 -out certs/jwt/trusted_jwt_signer.crt
+mkdir jwt
+openssl req -nodes -newkey rsa:2048 -keyout jwt/trusted_jwt_signer.key -out jwt/trusted_jwt_signer.csr -subj "/C=GB/ST=London/L=London/O=Global Security/OU=IT Department/CN=JWTSigner"
+openssl x509 -req -days 365 -in jwt/trusted_jwt_signer.csr -CA ca/ca.crt -CAkey ca/ca.key -set_serial 01 -out jwt/trusted_jwt_signer.crt
